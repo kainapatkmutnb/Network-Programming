3e96218d747ebe324090849800180576be5a9457
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2024-08-18 00:28:56 +0700

    "Updated app.js: changed variable declarations, added static asset serving, reformatted code, and added console log on server start"

--- Assign 1/video-chat/src/app.js
+++ Assign 1/video-chat/src/app.js
@@ -1,17 +1,22 @@
-let express = require('express');
-let app = express();
-let server = require('http').Server(app);
-let io = require('socket.io')(server);
-let stream = require('./ws/stream');
-let path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const stream = require('./ws/stream');
+const path = require('path');
 
+// Serve static assets from the 'assets' directory
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname+'/index.html');
+// Serve the index.html file at the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
+// Set up the '/stream' namespace and attach the stream handler
 io.of('/stream').on('connection', stream);
 
-server.listen(3000);
+// Start the server and listen on port 3000
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
