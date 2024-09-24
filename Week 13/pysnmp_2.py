5e921a9c9ac04550f38b6d8d0abf8740d1dbc3e8
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2024-09-24 12:06:57 +0700

    Convert SNMP queries to asynchronous using asyncio and hlapi for better performance.

--- Week 13/pysnmp_2.py
+++ Week 13/pysnmp_2.py
@@ -1,33 +1,38 @@
 import asyncio
 from pysnmp.hlapi.v3arch.asyncio import *
 
-# Asynchronous function to run SNMP get command
+# Asynchronous function to perform SNMP queries
 async def run():
+    # SNMP target host and community
+    host = '192.168.1.124'
+    community = 'public'
+    
     # Define SNMP OIDs for system name and uptime
     system_name_oid = ObjectType(ObjectIdentity('1.3.6.1.2.1.1.5.0'))
     system_uptime_oid = ObjectType(ObjectIdentity('1.3.6.1.2.1.1.3.0'))
 
-    # Perform the SNMP get command asynchronously
+    # SNMP command to get system name and uptime
     errorIndication, errorStatus, errorIndex, varBinds = await getCmd(
         SnmpEngine(),
-        CommunityData('public'),
-        await UdpTransportTarget.create(('192.168.1.124', 161)),  # Target IP address and port
+        CommunityData(community),
+        await UdpTransportTarget.create((host, 161)),  # Specify host and port
         ContextData(),
         system_name_oid,
         system_uptime_oid
     )
 
-    # Check for errors and print the results
+    # Check for errors and print out results
     if errorIndication:
-        print(errorIndication)  # Print error indication if any
-    elif errorStatus:
-        print('%s at %s' % (
-            errorStatus.prettyPrint(),  # Print error status
-            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'
-        ))
+        print(errorIndication)  # Print the error indication
     else:
-        for name, val in varBinds:
-            print('%s = %s' % (name.prettyPrint(), str(val)))  # Print the variable bindings
+        if errorStatus:
+            print('%s at %s' % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'
+            ))
+        else:
+            for name, val in varBinds:
+                print('%s = %s' % (name.prettyPrint(), str(val)))  # Print OID values
 
 # Run the async function
 asyncio.run(run())
